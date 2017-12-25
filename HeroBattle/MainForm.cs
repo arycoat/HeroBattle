e0475ce92d5a2b86e3a561d20e877d27c0639f30
jeonghyunseong <arycoat@naver.com>
2017-12-25 17:18:03 +0900

    room 추가

--- HeroBattle/MainForm.cs
+++ HeroBattle/MainForm.cs
@@ -17,9 +17,7 @@ namespace HeroBattle
 
         Timer timer = new Timer();
 
-        Map map;
-        Hero hero;
-        Hero enemy;
+        private Room room;
 
         public Form1()
         {
@@ -34,37 +32,14 @@ namespace HeroBattle
             datetime = DateTime.Now;
 
             //
-            map = new Map(10, 10);
-            map.Initialize();
-            map.load();
-
-            hero = new Hero();
-            hero.SetUp(map, Brushes.Blue);
-            hero.SetPosition(new Point(0, 0));
-
-            enemy = new Hero();
-            enemy.SetUp(map, Brushes.Red);
-            enemy.SetPosition(new Point(-1, -1));
-
-            //hero.endPos = enemy.GetPosition();
+            room = new Room();
+            room.Initialize();
         }
 
         private void Update(object sender, EventArgs e)
         {
             //
-            if (hero.IsEndMove())
-            {
-                Random random = new Random();
-                Point endPoint = new Point(random.Next(0, 9), random.Next(0, 9));
-                if (map.IsWalkable(endPoint.X, endPoint.Y) == true)
-                {
-                    enemy.SetPosition(endPoint);
-
-                    hero.endPos = enemy.GetPosition();
-                }
-            }
-
-            hero.Update(0);
+            room.Update();
 
             this.Invalidate();
         }
@@ -73,14 +48,7 @@ namespace HeroBattle
         {
             base.OnPaint(e);
 
-            map.OnPaint(e);
-            hero.OnPaint(e);
-            enemy.OnPaint(e);
-
-            //
-            //Font myFont = new System.Drawing.Font("Helvetica", 11, FontStyle.Italic);
-            //Brush myBrush = new SolidBrush(System.Drawing.Color.Red);
-            //e.Graphics.DrawString("E", myFont, myBrush, hero.endPos.X   * 50 + 10, hero.endPos.Y   * 50 + 30);
+            room.OnPaint(e);
         }
     }
 }
---
+++ HeroBattle/Room.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeroBattle
+{
+    class Room
+    {
+        private Map map;
+        private Hero hero;
+        private Hero enemy;
+
+        public Room()
+        {
+            map = new Map(10, 10);
+        }
+
+        public void Initialize()
+        {
+            map.Initialize();
+            map.load();
+
+            hero = new Hero();
+            hero.SetUp(map, Brushes.Blue);
+            hero.SetPosition(new Point(0, 0));
+
+            enemy = new Hero();
+            enemy.SetUp(map, Brushes.Red);
+            enemy.SetPosition(new Point(-1, -1));
+        }
+
+        public void Update()
+        {
+            //
+            if (hero.IsEndMove())
+            {
+                Random random = new Random();
+                Point endPoint = new Point(random.Next(0, 9), random.Next(0, 9));
+                if (map.IsWalkable(endPoint.X, endPoint.Y) == true)
+                {
+                    enemy.SetPosition(endPoint);
+
+                    hero.endPos = enemy.GetPosition();
+                }
+            }
+
+            hero.Update(0);
+        }
+
+        public void OnPaint(PaintEventArgs e)
+        {
+            map.OnPaint(e);
+            hero.OnPaint(e);
+            enemy.OnPaint(e);
+
+            //
+            //Font myFont = new System.Drawing.Font("Helvetica", 11, FontStyle.Italic);
+            //Brush myBrush = new SolidBrush(System.Drawing.Color.Red);
+            //e.Graphics.DrawString("E", myFont, myBrush, hero.endPos.X   * 50 + 10, hero.endPos.Y   * 50 + 30);
+        }
+    }
+}
