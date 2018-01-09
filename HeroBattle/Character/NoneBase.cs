1fe2b79ed02314d9d149d17751aaae19b5f1f060
jeonghyunseong <arycoat@naver.com>
2018-01-10 00:52:18 +0900

    NoneBase.cs add

---
+++ HeroBattle/Character/NoneBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroBattle
+{
+    public class NoneBase : CharacterState
+    {
+        public NoneBase()
+            : base(State.None)
+        {
+
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+}
